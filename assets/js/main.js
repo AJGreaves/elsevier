1cee45b0a6408cc3b01890fb00d7d9c24ba77ea7
Anna Greaves <gilhespy.anna@gmail.com>
2019-07-16 21:56:09 +0200

    Add email signup form for mailing list in frontend.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,8 @@ let confident = 0;
 let unsure = 0;
 let allQuestions = [];
 let questionData = [];
-let timer = 0;
 
 let formAnswer = document.querySelector('#formAnswer');
-
-
 
 fetch('assets/data/test.json')
     .then(function (data) {
@@ -22,13 +19,18 @@ fetch('assets/data/test.json')
     })
     .catch(err => console.log(err));
 
+$('#start-btn').click(function () {
+    $('#start-page').addClass('d-none');
+    $('#formAnswer').removeClass('d-none');
+})
+
 function getQuestion(allQuestions) {
     let question = allQuestions.find(x => x.id === activePage);
     return question;
 }
 
 function displayQuestion(questionData) {
-    
+
     let progressText = "Question " + activePage + "of 3"
     $('#progress-bar-text').text(progressText);
 
@@ -46,12 +48,12 @@ function displayQuestion(questionData) {
     return;
 }
 
-$('#confident-btn').click(function() {
+$('#confident-btn').click(function () {
     confident++;
     return;
 })
 
-$('#unsure-btn').click(function() {
+$('#unsure-btn').click(function () {
     unsure++;
     return;
 })
@@ -69,45 +71,50 @@ function respondAnswer(questionData, givenAnswer) {
 
     if (givenAnswer == questionData.answer) {
         correctAnswer(questionData, givenAnswer);
-    } 
-    else {
+    } else {
         incorrectAnswer(questionData, givenAnswer);
     }
 
+    // display info for each choice
     $('#option1-response-box .choice-response')
-    .text(questionData.choiceResponses.option1)
-    .removeClass('d-none');
+        .text(questionData.choiceResponses.option1)
+        .removeClass('d-none');
 
     $('#option2-response-box .choice-response')
-    .text(questionData.choiceResponses.option2)
-    .removeClass('d-none');
+        .text(questionData.choiceResponses.option2)
+        .removeClass('d-none');
 
     $('#option3-response-box .choice-response')
-    .text(questionData.choiceResponses.option3)
-    .removeClass('d-none');
+        .text(questionData.choiceResponses.option3)
+        .removeClass('d-none');
 
     $('#option4-response-box .choice-response')
-    .text(questionData.choiceResponses.option4)
-    .removeClass('d-none');
+        .text(questionData.choiceResponses.option4)
+        .removeClass('d-none');
 
     $('#option5-response-box .choice-response')
-    .text(questionData.choiceResponses.option5)
-    .removeClass('d-none');
+        .text(questionData.choiceResponses.option5)
+        .removeClass('d-none');
 
+    // display icons to indicate correct / incorrect answer
     let correctCircle = '#circle-' + questionData.answer;
     $(correctCircle)
-    .addClass('fa-check-circle')
-    .removeClass('fa-times-circle')
-    .parent().removeClass('d-none');
+        .addClass('fa-check-circle')
+        .removeClass('fa-times-circle')
+        .parent().removeClass('d-none');
 
+    // make selected radio color grey
     $('.radio-orange').addClass('radio-grey').removeClass('radio-orange');
 
+    // display data in side info bar
     $('#percent-correct').text(questionData.percentCorrect);
 
+    // change visible buttons at bottom of page
     $('#confident-btn, #unsure-btn').addClass('d-none');
     $('#next-btn').removeClass('d-none');
     $('.radio').css('pointer-events', 'none');
 
+    // show side info bar
     $('#question-column').removeClass('col-12').addClass('col-9');
     $('#side-info-bar').removeClass('d-none');
 
@@ -116,9 +123,9 @@ function respondAnswer(questionData, givenAnswer) {
 
 function correctAnswer(questionData) {
     $('#question-response')
-    .text('Your answer is correct.')
-    .css('color', '#008000')
-    .removeClass('d-none');
+        .text('Your answer is correct.')
+        .css('color', '#008000')
+        .removeClass('d-none');
 
     let correctMsg = '#' + questionData.answer + '-response-box .radio-response';
     $(correctMsg).text('Correct').css('color', '#015f06');
@@ -127,15 +134,15 @@ function correctAnswer(questionData) {
 
 function incorrectAnswer(questionData, givenAnswer) {
     $('#question-response')
-    .text('Your answer is incorrect.')
-    .css('color', '#c20606')
-    .removeClass('d-none');
+        .text('Your answer is incorrect.')
+        .css('color', '#c20606')
+        .removeClass('d-none');
 
     let incorrectCircle = '#circle-' + givenAnswer;
     $(incorrectCircle)
-    .addClass('fa-times-circle')
-    .removeClass('fa-check-circle')
-    .parent().removeClass('d-none');
+        .addClass('fa-times-circle')
+        .removeClass('fa-check-circle')
+        .parent().removeClass('d-none');
 
     let incorrect = '#' + givenAnswer + '-response-box .radio-response';
     let correct = '#' + questionData.answer + '-response-box .radio-response';
@@ -144,15 +151,21 @@ function incorrectAnswer(questionData, givenAnswer) {
     return;
 }
 
-$('#next-btn').click(function() {
+$('#next-btn').click(function () {
     activePage++;
-    resetPage();
+    if (activePage <= 3) {
+        resetPage();
+    } else {
+        $('#formAnswer').addClass('d-none');
+        $('#email-signup-page').removeClass('d-none');
+        console.log('quiz complete!');
+    }
 })
 
 function resetPage() {
     $('.radio').css('pointer-events', 'auto');
     $('.circle-wrapper, .radio-response, .choice-response, #question-response')
-    .addClass('d-none');
+        .addClass('d-none');
 
     $('#confident-btn, #unsure-btn').removeClass('d-none');
     $('#next-btn').addClass('d-none');
@@ -168,4 +181,3 @@ function resetPage() {
     displayQuestion(questionData);
     return;
 }
-
