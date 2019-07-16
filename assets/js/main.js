82392943a78943fd23b2116bf2b9c4a3f6a10d1c
Anna Greaves <gilhespy.anna@gmail.com>
2019-07-16 12:33:44 +0200

    Question and possible answers displaying correctly.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@ fetch('assets/data/test.json')
         return data.json();
     })
     .then(function (data) {
-        let question = getQuestion(data);
-        console.log(question);
+        let questionData = getQuestion(data);
+        displayQuestion(questionData);
     });
 
 let activePage = 1;
@@ -12,4 +12,19 @@ let activePage = 1;
 function getQuestion(data) {
     let question = data.find(x => x.id === activePage);
     return question;
+}
+
+function displayQuestion(questionData) {
+    
+    let progressText = "Question " + activePage + "of 3"
+    $('#progress-bar-text').text(progressText);
+
+    $('#questionIntro').text(questionData.questionIntro);
+    $('#question').text(questionData.question);
+    $('#option1Label').text('A: ' + questionData.choices.option1);
+    $('#option2Label').text('B: ' + questionData.choices.option2);
+    $('#option3Label').text('C: ' + questionData.choices.option3);
+    $('#option4Label').text('D: ' + questionData.choices.option4);
+    $('#option5Label').text('E: ' + questionData.choices.option5);
+    
 }
